1e7f51bdf66c97ad0796b681b5ce540cea9afcf6
Kenneth Sullivan <90344298+kmsull@users.noreply.github.com>
2025-01-07 09:26:30 -0500

    Update navBar.tsx
    Added Image Import

--- app/emconstruction/src/app/Components/navBar.tsx
+++ app/emconstruction/src/app/Components/navBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../globals.css'
 import Link from 'next/link'
+import Image from 'next/image';
 import logo from '../assets/images/EMConstructionLogo.png';
 
 const NavBar = () => {
