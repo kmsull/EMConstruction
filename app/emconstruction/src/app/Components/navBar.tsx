c6eff0ae3615150ff017b22a06fd124db72cc482
Kenneth Sullivan <90344298+kmsull@users.noreply.github.com>
2025-01-07 09:20:22 -0500

    Update navBar.tsx
    Updated Logo into Navbar

--- app/emconstruction/src/app/Components/navBar.tsx
+++ app/emconstruction/src/app/Components/navBar.tsx
@@ -8,10 +8,9 @@ const NavBar = () => {
         <nav className="flex gap-4 p-4 bg-black-200">
             <ul className="flex gap-4">
                 <li>
-                    <image href="../img/EMConstructionLogo.png"></image>
-                </li>
-                <li>
-                    <Link className="text-white-500" href="/">Home</Link>
+                    <Link href="/">
+                        <Image className='flex items-center' src={logo} alt="Home / EMCon Logo" width={64} height={64} />
+                    </Link>
                 </li>
                 <li>
                     <Link className="text-white-500" href="/Portfolio">Services</Link>
